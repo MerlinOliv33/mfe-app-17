d974b503e979ac61fd55deb46b72188b200826e8
pdcr859 <olivier.thibaud@labanquepostale.fr>
2024-02-28 07:45:54 +0000

    integration-mfe

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  name: 'mfeApp17',
+  exposes: {
+    './Component': './src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
